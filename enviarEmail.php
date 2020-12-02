76a2849ea6d226ff47d6b7e9b627259dad607afb
hthenrique <ht.henrique@live.com>
2020-12-01 22:21:23 -0300

    envios de email

--- enviarEmail.php
+++ enviarEmail.php
@@ -10,7 +10,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
 $nome = $_POST['Nome'];
 $email = $_POST['EmailUsuario'];
 $menssagem = $_POST['Menssagem'];
-$para = "enge.wt@gmail.com";
+$para = "contato@projeplanengenharia.com.br";
 
 $corpo = "\nNome: ".$nome."\r\n".
       "\nE-mail: ".$email."\r\n".
@@ -19,14 +19,14 @@ $corpo = "\nNome: ".$nome."\r\n".
 $mail = new PHPMailer;
 $mail->isSMTP(); 
  // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
-$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
-$mail->Port = 587; // TLS only
-$mail->SMTPSecure = 'tls'; // ssl is deprecated
+$mail->Host = "mail.projeplanengenharia.com.br"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Port = 465; // TLS only
+$mail->SMTPSecure = 'ssl'; // ssl is deprecated
 $mail->SMTPAuth = true;
-$mail->Username = 'enge.wt@gmail.com'; // email
-$mail->Password = 'Bmbjdqhbm2097'; // password
+$mail->Username = 'contato@projeplanengenharia.com.br'; // email
+$mail->Password = 'Will1991!'; // password
 $mail->setFrom($_POST['EmailUsuario'], $_POST['Nome']); // From email and name
-$mail->addAddress($para, 'William Texeira'); // to email and name
+$mail->addAddress($para, 'Projeplan Engenharia'); // to email and name
 $mail->Subject = 'Contato do Site';
 $mail->msgHTML("<html>Nome: {$nome}<br>Email: {$email}<br><br>Menssagem:<br>{$menssagem}</html>"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
 $mail->AltBody = 'HTML messaging not supported';
@@ -40,10 +40,10 @@ $mail->SMTPOptions = array(
   );
 
   if(!$mail->send()){
-      echo "Mailer Error: " . $mail->ErrorInfo;
+      header ("location:index.php?mail=erro");
   }else{
-      echo "<script>alert('Email enviado com sucesso')</script>";
-      header(sprintf('location: %s', $_SERVER['https://wt-engenharia.000webhostapp.com']));
+      //echo "<script>alert('Email enviado com sucesso')</script>";
+      header ("location:index.php?mail=sucesso");
       exit;
   }
 ?>
